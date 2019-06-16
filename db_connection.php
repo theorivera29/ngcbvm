e79b4f1654a6059dc13ccac4138f815f0a67045e
theorivera29 <47031466+theorivera29@users.noreply.github.com>
2019-06-17 04:24:22 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
     $host = '127.0.0.1';
 	$user = 'root';
-	$pass = '';
+	$pass = 'ngcbdc';
 	$db = 'ngcbdc';
 	$conn = mysqli_connect($host, $user, $pass, $db) or die('Cannot connect to db');
 	date_default_timezone_set('Asia/Manila');
